7fc018f0e14e872f94dffd99a86e2123d9bfe74a
Nohier <nohierassan2@gmail.com>
2020-02-23 21:30:43 +0200

    Models and user modifications

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Tag, UserModel
+from .models import Post, Comment, Tag
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Tag)
-admin.site.register(UserModel)
+
--- Project/settings.py
+++ Project/settings.py
@@ -126,9 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+# to reference the static files and where the post images will be stored 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-           os.path.join(BASE_DIR, "static"),
-)
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 # to redirect the user to the home page after login
 LOGIN_REDIRECT_URL = '/blog/home'
