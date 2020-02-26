277e8d67443968ca758e41d4fde7aae06b7987ec
mayar alaa <mayar.alaa188@gmail.com>
2020-02-26 11:20:56 +0200

    modifying comments

--- post/forms.py
+++ post/forms.py
@@ -2,7 +2,8 @@
 # from django import forms
 
 
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('name', 'email', 'body')
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
